649886c41f716ff1062efb5577db3e2a5f7e853b
Alex Pickering <alexvpickering@gmail.com>
2021-02-08 18:23:27 -0800

    dseqr deploy button works

---
+++ assets/js/custom.js
@@ -0,0 +1,43 @@
+const deployButton = document.getElementById("deploy");
+const deployProgress = document.getElementById("deploy-progress");
+const deployProgressBar = document.getElementById("deploy-progress-bar");
+
+deployButton.addEventListener("click", (event) => {
+  deployCountdown();
+});
+
+const deployDseqr = () => {};
+
+const deployCountdown = () => {
+  var isDeployed = false;
+  const http = new XMLHttpRequest();
+  const url =
+    "https://jdy8c3206d.execute-api.us-east-2.amazonaws.com/default/CDKLambdaDockerStack-functionF19B1A04-XCOBCMG259NY";
+  http.open("GET", url);
+  http.send();
+
+  http.onreadystatechange = (e) => {
+    isDeployed = true;
+    console.log(http.responseText);
+  };
+
+  var i = 0;
+  var totaltime = 60 * 6;
+  const interval = setInterval(function () {
+    i += 1;
+    var timeleft = totaltime - i;
+    var percent = ((totaltime - timeleft) / totaltime) * 100;
+
+    var result = new Date(timeleft * 1000).toISOString().substr(15, 4);
+    deployButton.innerHTML = result;
+    deployProgressBar.style.width = percent + "%";
+
+    if ((isDeployed && timeleft > 60 * 5) || timeleft == 0) {
+      deployButton.innerHTML = "Open Dseqr";
+      deployButton.href = "https://drugseqr.com";
+      deployProgressBar.style.width = "100%";
+      deployProgress.style.visibility = "hidden";
+      clearInterval(interval);
+    }
+  }, 1000); //calling it every 1 second
+};
--- content/_index.md
+++ content/_index.md
@@ -1,7 +1,7 @@
 ---
-title : "Modern Documentation Theme"
+title : "End-To-End RNA-Seq Analysis"
 description: "Doks is a Hugo theme helping you build modern documentation websites that are secure, fast, and SEO-ready — by default."
-lead: "Doks is a Hugo theme helping you build modern documentation websites that are secure, fast, and SEO-ready — by default."
+lead: "Dseqr is a web application that helps you run end-to-end fastq-to-pathways single-cell and bulk RNA-seq analyses."
 date: 2020-10-06T08:47:36+00:00
 lastmod: 2020-10-06T08:47:36+00:00
 draft: false

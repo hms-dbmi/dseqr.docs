e9817399245ea05c114bbc60683751edb8fd23c3
Alex Pickering <alexvpickering@gmail.com>
2021-02-09 16:58:18 -0800

    testing

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,21 +3,25 @@ const deployProgress = document.getElementById("deploy-progress");
 const deployProgressBar = document.getElementById("deploy-progress-bar");
 
 deployButton.addEventListener("click", (event) => {
-  deployCountdown();
+  checkDeployed();
+  // deployCountdown();
 });
 
-const deployCountdown = () => {
-  var isDeployed = false;
+const checkDeployed = () => {
+  // check if site is active
   const http = new XMLHttpRequest();
-  const url =
-    "https://jdy8c3206d.execute-api.us-east-2.amazonaws.com/default/CDKLambdaDockerStack-functionF19B1A04-XCOBCMG259NY";
-  http.open("GET", url);
+  const url = "https://drugseqr.com";
+  http.open("OPTIONS", url);
   http.send();
 
   http.onreadystatechange = (e) => {
-    isDeployed = true;
     console.log(http.responseText);
   };
+};
+
+const deployCountdown = () => {
+  const url =
+    "https://jdy8c3206d.execute-api.us-east-2.amazonaws.com/default/CDKLambdaDockerStack-functionF19B1A04-RVIOF18XPMZM";
 
   var i = 0;
   var totaltime = 60 * 6;
@@ -30,7 +34,7 @@ const deployCountdown = () => {
     deployButton.innerHTML = result;
     deployProgressBar.style.width = percent + "%";
 
-    if ((isDeployed && timeleft > 60 * 5) || timeleft == 0) {
+    if (timeleft == 0) {
       deployButton.innerHTML = "Open Dseqr";
       deployButton.href = "https://drugseqr.com";
       deployProgressBar.style.width = "100%";

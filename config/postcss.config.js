1773f1a489cbc4cc40bb1f82f0c1501a8e03b474
Alex Pickering <alexvpickering@gmail.com>
2021-02-13 06:35:33 -0800

    add dots

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,23 +1,20 @@
-const autoprefixer = require('autoprefixer');
-const purgecss = require('@fullhuman/postcss-purgecss');
-const whitelister = require('purgecss-whitelister');
+const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const whitelister = require("purgecss-whitelister");
 
 module.exports = {
   plugins: [
     autoprefixer(),
     purgecss({
-      content: [
-        './layouts/**/*.html',
-        './content/**/*.md',      
-      ],
+      content: ["./layouts/**/*.html", "./content/**/*.md"],
       safelist: [
-        'lazyloaded',
+        "lazyloaded",
         ...whitelister([
-          './assets/scss/components/_code.scss',
-          './assets/scss/components/_search.scss',
-          './assets/scss/common/_dark.scss',
+          "./assets/scss/components/_code.scss",
+          "./assets/scss/components/_search.scss",
+          "./assets/scss/common/_dark.scss",
         ]),
       ],
     }),
   ],
-}
+};
